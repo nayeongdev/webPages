7f829a1871cc37a4469264dd6e0c5ac76af05f05
nayeongdev <nayeongkim.dev@gmail.com>
2023-01-29 16:41:16 +0900

    jquery사용

--- colors.js
+++ colors.js
@@ -1,24 +1,17 @@
 let Body = {
   setColor: function (color) {
-    document.querySelector('body').style.color = color;
+    $('body').css('color', color);
   },
   setBackgroundColor: function (color) {
-    document.querySelector('body').style.backgroundColor = color;
+    $('body').css('backgroundColor', color);
   }
 }
 let Links = {
   setColor: function (color) {
-    let alist = document.querySelectorAll('a');
-    let i = 0;
-    while (i < alist.length) {
-      alist[i].style.color = color;
-      i++;
-    }
+    $('a').css('color', color); 
   }
 }
 function nightDayHandler(self) {
-  let target = document.querySelector('body');
-
   if (self.textContent === 'night') {
     Body.setBackgroundColor('black');
     Body.setColor('white');
