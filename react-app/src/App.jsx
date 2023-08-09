7b86e95e4f296b4320e81fd1a385a423cee97e7b
nayeongdev <nayeongkim.dev@gmail.com>
2023-08-09 16:32:23 +0900

    리액트 - 이벤트
    리액트에서 이벤트 기능을 구현하는 방법
    이벤트는 Props를 이용한다.
    event.preventDefault()를 이용해서 기본동작을 비활성화 할 수 있다.

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -2,14 +2,22 @@ import './App.css';
 function Header(props) {
   // console.log('props', props, props.title);
   return <header>
-    <h1><a href="/">{props.title}</a></h1>
+    <h1><a href="/" onClick={event => {
+      event.preventDefault();
+      props.onChangeMode();
+    }}>{props.title}</a></h1>
   </header>
 }
 function Nav(props) {
   const lis = []
   for (let i = 0; i < props.topics.length; i++) {
     const topic = props.topics[i];
-    lis.push(<li key={topic.id}><a href={'/read/' + topic.id}>{topic.title}</a></li>)
+    lis.push(<li key={topic.id}>
+      <a id={topic.id} href={'/read/' + topic.id} onClick={event => {
+        event.preventDefault();
+        props.onChangeMode(event.target.id);
+      }}>{topic.title}</a>
+    </li>)
   }
   return <nav>
     <ol>
@@ -31,8 +39,12 @@ function App() {
   ]
   return (
     <div>
-      <Header title="WEB"></Header>
-      <Nav topics={topics}></Nav>
+      <Header title="WEB" onChangeMode={() => {
+        alert('Header');
+      }}></Header>
+      <Nav topics={topics} onChangeMode={id => {
+        alert(id);
+      }}></Nav>
       <Article title="Welcome" body="Hello, WEB"></Article>
     </div>
   );
